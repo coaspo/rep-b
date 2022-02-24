d3d40aaf519c9cb995920bb49dca0d8a940e6e69
coaspo <coaspo@gmail.com>
2022-02-24 18:36:15 -0500

    'br1'

--- w/apps/webscrape/cli.py
+++ w/apps/webscrape/cli.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 import twiv_picks
 import hacker_news_sort
+import dictionary_look_up
 import tkinter as tk
 
 root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
+root.title("Web scrapping / Web APIs")
+root.configure(background='white')
+root.geometry("450x350")
 
-button = tk.Button(frame,
-                   text="TWIV picks, easily scanned list",
-                   fg="red",
-                   command=twiv_picks.main)
-button.pack(side=tk.TOP)
-slogan = tk.Button(frame,
-                   text="Hacker news, sorted by points",
-                   command=hacker_news_sort.main)
-slogan.pack(side=tk.TOP)
+i=1
+x0 = 50
+dy=50
+
+label = tk.Label(root)
+label.place(x = x0, y = 4*dy +dy//2, width=350, height=30)
+def f1():
+   dictionary_look_up.run(label)
+
+b = tk.Button(root, bg='#b1ffa3', command=dictionary_look_up.main,
+                   text="Dictionary word-list look-up (API)")
+b.place(x = x0, y = i*dy, width=350, height=30)
+
+i+=1
+b = tk.Button(root, bg='#b1ffa3', command=hacker_news_sort.main,
+                   text="Hacker news; sort last week's links")
+b.place(x = x0, y = i*dy, width=350, height=30)
+
+i+=1
+b = tk.Button(root, bg='#b1ffa3', command=twiv_picks.main,
+                   text="TWIV picks; create different list types")
+b.place(x = x0, y = i*dy, width=350, height=30)
+
+i+=1
+label = tk.Label(root, text = 'Check Terminal window after clicking a button')
+label.place(x = x0, y = i*dy+20, width=350, height=15)
 
 root.mainloop()
+
--- w/apps/webscrape/dictionary_look_up.py
+++ w/apps/webscrape/dictionary_look_up.py
@@ -1,55 +1,92 @@
 #!/usr/bin/env python3
-
 import requests
 import json
 
 DICTIONARY_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
-
+DEBUG = False
 
 def input_words():
+    print ('\n Enter word(s) \
+      \n   add dash (-) prefix to first word to suppress example/origin,\
+      \n   hit Return twice when done,\
+      \n Or enter a non-letter and Enter to exit.\n')
     words = []
+    definition_only = False
     while True:
       line = input()
-      if line == '' or not line[0].isalpha():
+      if line == '' or (len(line) == 1 and not line[0].isalpha()):
         break
+      if line[0] == '-':
+        definition_only = True
+        line = line[1:]
       multi_words = line.split(" ")
       for word in multi_words:
         word = word.strip()
         if word != '':
           if not word.isalpha():
-            break
+            print('Rejected invalid word:', word)
+            continue
           words.append(word)
-    return words
+    if DEBUG:
+      print('definition_only=', definition_only,'\n  words=', words)
+    return definition_only, words
 
 def get_dictionary_page(word: str):
     url = DICTIONARY_URL + word
     page = requests.get(url, headers='')
-    return page.text
+    text = page.text
+    if DEBUG:
+      print('text=', text)
+    return text
 
 def parse_definition(text: str):
     d =json.loads(text)[0]
-    definition = d["meanings"][0]["definitions"][0]["definition"]
-    return definition
+    definitions = d["meanings"][0]["definitions"][0]
+    definition = definitions["definition"]
+    example = definitions["example"]  if 'example' in definitions.keys() \
+              else '-'
+    origin = d["origin"]  if 'origin' in d.keys() \
+              else '-'
+    if DEBUG:
+      print('definition=', definition, '\n  example=', example,'\n  origin=', origin)
+    return definition, example, origin
 
 def lookup_definitions(words: list):
     definitions = []
+    examples = []
+    origins = []
     for word in words:
       text = get_dictionary_page(word)
-      definition = parse_definition(text)
+      definition, example, origin = parse_definition(text)
       definitions.append(definition)
-    return definitions
+      examples.append(example)
+      origins.append(origin)
+    if DEBUG:
+      print('definitions=', definitions, '\n  examples=', examples,'\n  origins=', origins)
+    return definitions, examples, origins
+
+def print_definitions(words, definitions, *args):
+    for i in range(len(words)):
+      print(words[i], ':',  definitions[i])
+      if len(args) == 0:
+        continue
+      if args[0][i] != '-':
+        print(' ' * len(words[i]), ' ',  args[0][i])
+      if args[1][i] != '-':
+        print(' ' * len(words[i]), ' ',  args[1][i])
 
 def main():
     try:
         while True:
-          print ('\n Enter word(s), space or new line delimited,\n\
-      or a non-letter to exit.\n')
-          words = input_words()
+          definition_only, words = input_words()
           if len(words) == 0:
+            print('Done')
             exit(0)
-          definitions = lookup_definitions(words)
-          for i in range(len(words)):
-            print(words[i], ': ',  definitions[i])
+          definitions, examples, origins = lookup_definitions(words)
+          if definition_only:
+            print_definitions(words, definitions)
+            continue
+          print_definitions(words, definitions, examples, origins)
     except Exception as exc:
         print(exc)
         import traceback
